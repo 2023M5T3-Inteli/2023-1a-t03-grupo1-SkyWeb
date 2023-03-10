4d817c1fe65dd002a80b92b4b8a17be4a57d4eba
raissa2861 <raissa.sabino@sou.inteli.edu.br>
2023-03-10 14:59:51 -0300

    cade meu diplomaaa
    nova branche feita em 15 min

--- src/frontend/pages/Allprojects/index.jsx
+++ src/frontend/pages/Allprojects/index.jsx
@@ -2,20 +2,41 @@ import * as React from 'react';
 import { Typography } from "@mui/material";
 import { Filter } from "../../components/filter"
 import { Menu } from "../../components/menu";
-export function Allprojects(){
+import { ActionAreaCard } from "../../components/projectCard"
+
+
+
+export function Allprojects() {
+
+    const array = [{ status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "C++ ", "PHP"] }, { status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "Python", "Ruby"] }, { status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "Python", "Ruby"] }, { status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "Python", "Ruby"] }, { status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "Python", "Ruby"] }, { status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "Python", "Ruby"] }, { status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "Python", "Ruby"] }, { status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "Python", "Ruby"] }, { status: "Open", nome: "teste card", deadline: "02/08/23", area: "Finance", duration: "3 Months", tags: ["Java", "Python", "Ruby"] }]
+
 
     return (
         <div style={{ display: "flex" }}>
-            <Menu userName="Chloe Price" userRole="Developer"/>
-            <div style={{ display: 'flex', justifyContent: 'flex-start', flexDirection: 'column'}}>
-                <div id="title" style={{ paddingLeft:"50px", paddingTop: "20px" }}>
-                    <h1 style={{ fontFamily: "Poppins",  }}>All projects</h1>
-                    <h3>Find a project that fits to you and apply!</h3>
-                </div>
-                <Filter />
+            <div style={{ height: "300px" }}>
+                <Menu userName="Chloe Price " userRole="Developer" />
             </div>
 
-            
+            <div style={{ display: 'flex', justifyContent: 'flex-start', flexDirection: 'column' }}>
+
+                <div id="title" style={{ paddingLeft: "50px", paddingTop: "20px" }}>
+                    <h1 style={{ fontFamily: "Poppins", fontSize: "30px" }}>All projects</h1>
+                    <h3>Find a project that fits to you and apply!</h3>
+                </div>
+
+                <Filter />
+
+                <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", overflowY: "scroll", width: "950px", height: "385px" }} >
+                    {array.map((item) => {
+                        return (
+                            <ActionAreaCard status={item.status} name={item.nome} area={item.area} deadLine={item.deadline} duration={item.duration} tags={item.tags} />
+                        )
+                    })}
+
+                </div>
+
+            </div>
+
         </div>
     )
 }
