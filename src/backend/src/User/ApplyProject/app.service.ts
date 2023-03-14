2e75dbc153bcc09513f3b938193b0cdcf9848476
0rdep12 <78965293+0rdep12@users.noreply.github.com>
2023-03-14 16:26:33 -0300

    ApplyProject completed
    edition in:
    -modelSelect, modelCreate
    -User/app.module.ts
    -User/applyProject

---
+++ src/backend/src/User/ApplyProject/app.service.ts
@@ -0,0 +1,73 @@
+import { ModelCreate } from "src/models/modelCreate";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common"
+import { ModelSelect } from "src/models/modelSelect";
+
+@Injectable()
+export class ServiceApplyProject {
+    constructor(private readonly modelCreate: ModelCreate, private readonly modelSelect: ModelSelect) { }
+
+    async execute(_idProject: number, _idUser: number, _idRole: number) {
+        const userApply = await this.modelSelect.findUserApplyProjectByIdUserAndIdProject(_idProject, _idUser, _idRole)
+        const userRole = await this.modelSelect.findUserApplyProjectByIdRoleAndIdProject(_idRole, _idProject)
+        const userExist = await this.modelSelect.findUserApplyProjectExistUser(_idUser)
+        const projectExist = await this.modelSelect.findUserApplyProjectExistProject(_idProject)
+        const roleExist = await this.modelSelect.findUserApplyProjectExistRole(_idRole)
+
+        if (userApply.length != 0) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.AMBIGUOUS,
+                    error: 'Ambigous id in user, project or role',
+                },
+                HttpStatus.AMBIGUOUS,
+            );
+        }
+
+        if (userRole.length != 0) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.NOT_FOUND,
+                    error: 'Not exist project or role with this ID',
+                },
+                HttpStatus.NOT_FOUND,
+            );
+        }
+
+        if (userExist.length === 0) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.NOT_FOUND,
+                    error: 'User does not exist'
+                },
+                HttpStatus.NOT_FOUND,
+            );
+        }
+
+        if (projectExist.length === 0) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.NOT_FOUND,
+                    error: 'Project does not exist'
+                },
+                HttpStatus.NOT_FOUND,
+            );
+        }
+
+        if (roleExist.length === 0) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.NOT_FOUND,
+                    error: 'Role does not exist'
+                },
+                HttpStatus.NOT_FOUND,
+            );
+        }
+
+        const result = this.modelCreate.applyProject(_idProject, _idUser, _idRole)
+        return result
+
+    }
+
+}
+
+
