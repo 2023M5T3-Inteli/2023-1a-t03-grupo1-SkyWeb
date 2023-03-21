531ff55508e8e4ea707550b27ae45893e910d5e5
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2023-03-21 14:58:57 -0300

    FIX

--- src/backend/src/User/ApplyProject/app.controller.ts
+++ src/backend/src/User/ApplyProject/app.controller.ts
@@ -1,35 +1,35 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
-import { DTOBodyApplyProject } from './DTO/DtoApplyProject';
+import { DTOBodyApplyProject } from 'src/User/ApplyProject/DTO/DTOApplyProject';
 import { ServiceApplyProject } from './app.service';
 import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 @ApiTags('User')
 export class PostApplyProject {
-    constructor(private readonly serviceApplyProject: ServiceApplyProject) { }
+    constructor(private readonly serviceApplyProject: ServiceApplyProject) {}
 
     @Post('applyProject')
     @ApiOperation({ summary: 'Apply for a project' })
     @ApiResponse({
         status: 201,
-        description:
-            '[{idUser:number,idProject:number,idRole:number}]',
+        description: '[{idUser:number,idProject:number,idRole:number}]',
     })
     @ApiResponse({
         status: 300,
-        description:
-            'Ambigous id in user, project or role',
+        description: 'Ambigous id in user, project or role',
     })
     @ApiResponse({
         status: 404,
-        description:
-            'User, project or role does not exist',
+        description: 'User, project or role does not exist',
     })
-
     async applyProject(@Body() body: DTOBodyApplyProject) {
-        const { idProject, idRole, idUser } = body
-        const result = await this.serviceApplyProject.execute(idProject, idUser, idRole);
+        const { idProject, idRole, idUser } = body;
+        const result = await this.serviceApplyProject.execute(
+            idProject,
+            idUser,
+            idRole,
+        );
         return result;
     }
 }
